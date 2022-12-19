bd59a5dc5ce2899d176dd01af78592ee4cf0c967
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-12-19 19:11:47 +0100

    Add solution to task 4

---
+++ task4/creatingCatalogWithinAPI.js
@@ -0,0 +1,42 @@
+const request = require('request');
+
+const baseUrl = 'https://api.exponea.com';
+const projectId = '37b21e42-9cc9-11e9-9bbf-aa978f2cdad2';
+
+const options = {
+	method: 'POST',
+	url: `${baseUrl}/data/v2/projects/${projectId}/catalogs`,
+	headers: {
+		accept: 'application/json',
+		authorization:
+			'Basic cXF4Zzc5YnB2OHp3ajk1cTk3cmkwdDZvem44enE2cms0YTR1Z3d6bnU0ejNlMXJobXZ1dHF0MnQ3YzVzbzJ5ZTpndnBram1venA3YXR6MDcxd3Y1eHVrODl5dnBtcmozeHo3bGhlemNlM2x0ZWFzODA2cHg5cmNxMTB5cjMzdTUy',
+		'Content-type': 'application/json',
+	},
+	body: {
+		name: 'Exotic Cars',
+		is_product_catalog: 'true',
+		fields: [
+			{
+				name: 'lamborghini',
+				id: '1',
+				model: 'aventador svj',
+				engine: 'v12',
+				type: 'string',
+			},
+			{
+				name: 'ferrari',
+				id: '2',
+				model: 'sf90 stradale',
+				engine: 'v8',
+				type: 'string',
+			},
+		],
+	},
+	json: true,
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
